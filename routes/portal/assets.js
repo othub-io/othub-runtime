d4a63c66fd1b7463e8ebddad97bf978b95a9fa83
CosmiCloud <tybranha@gmail.com>
2023-07-30 22:45:45 -0400

    run time bug fixes and changes for front end

--- routes/portal/assets.js
+++ routes/portal/assets.js
@@ -1,114 +1,116 @@
-require('dotenv').config()
-var express = require('express')
-var router = express.Router()
-const mysql = require('mysql')
-const purl = require('url')
+require("dotenv").config();
+var express = require("express");
+var router = express.Router();
+const mysql = require("mysql");
+const purl = require("url");
 const otp_connection = mysql.createConnection({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
-  database: process.env.SYNC_DB
-})
+  database: process.env.SYNC_DB,
+});
 
-function executeOTPQuery (query, params) {
+function executeOTPQuery(query, params) {
   return new Promise((resolve, reject) => {
     otp_connection.query(query, params, (error, results) => {
       if (error) {
-        reject(error)
+        reject(error);
       } else {
-        resolve(results)
+        resolve(results);
       }
-    })
-  })
+    });
+  });
 }
 
-async function getOTPData (query, params) {
+async function getOTPData(query, params) {
   try {
-    const results = await executeOTPQuery(query, params)
-    return results
+    const results = await executeOTPQuery(query, params);
+    return results;
   } catch (error) {
-    console.error('Error executing query:', error)
-    throw error
+    console.error("Error executing query:", error);
+    throw error;
   }
 }
 
 /* GET explore page. */
-router.get('/', async function (req, res, next) {
-  ip = req.socket.remoteAddress
+router.get("/", async function (req, res, next) {
+  ip = req.socket.remoteAddress;
   if (process.env.SSL_KEY_PATH) {
-    ip = req.headers['x-forwarded-for']
+    ip = req.headers["x-forwarded-for"];
   }
 
-  url_params = purl.parse(req.url, true).query
+  url_params = purl.parse(req.url, true).query;
 
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-  query = `select * from v_pubs`
-  conditions = []
-  params = []
+  query = `select * from v_pubs`;
+  conditions = [];
+  params = [];
 
-  limit = 100
+  limit = 100;
   if (url_params.limit && Number(url_params.limit)) {
-    limit = url_params.limit
+    limit = url_params.limit;
   }
 
   if (url_params.nodeId && Number(url_params.nodeId)) {
-    nodeId = Number(url_params.nodeId)
-    conditions.push(`winners like ? OR winners like ? OR winners like ?`)
+    nodeId = Number(url_params.nodeId);
+    conditions.push(`winners like ? OR winners like ? OR winners like ?`);
 
-    nodeId = `%"${nodeId},%`
-    params.push(nodeId)
+    nodeId = `%"${nodeId},%`;
+    params.push(nodeId);
 
-    nodeId = `%,${nodeId},%`
-    params.push(nodeId)
+    nodeId = `%,${nodeId},%`;
+    params.push(nodeId);
 
-    nodeId = `%,${nodeId}"%`
-    params.push(nodeId)
+    nodeId = `%,${nodeId}"%`;
+    params.push(nodeId);
   }
-  
+
   if (url_params.publisher) {
-    conditions.push(`publisher = ?`)
-    params.push(url_params.publisher)
+    conditions.push(`publisher = ?`);
+    params.push(url_params.publisher);
   }
 
-  order_by = 'block_ts_hour'
+  order_by = "block_ts_hour";
   if (url_params.order) {
-    order_by = url_params.order
+    order_by = url_params.order;
   }
 
   if (url_params.ual) {
-    conditions.push(`ual = ?`)
-    params.push(url_params.ual)
+    conditions.push(`ual = ?`);
+    params.push(url_params.ual);
   }
 
-  whereClause = conditions.length > 0 ? 'WHERE ' + conditions.join(' AND ') : ''
-  sqlQuery = query + ' ' + whereClause + ` order by ${order_by} desc LIMIT ${limit}`
+  whereClause =
+    conditions.length > 0 ? "WHERE " + conditions.join(" AND ") : "";
+  sqlQuery =
+    query + " " + whereClause + ` order by ${order_by} desc LIMIT ${limit}`;
 
-  v_pubs = ''
-  console.log(sqlQuery)
+  v_pubs = "";
+  console.log(sqlQuery);
   v_pubs = await getOTPData(sqlQuery, params)
-    .then(results => {
+    .then((results) => {
       //console.log('Query results:', results);
-      return results
+      return results;
       // Use the results in your variable or perform further operations
     })
-    .catch(error => {
-      console.error('Error retrieving data:', error)
-    })
+    .catch((error) => {
+      console.error("Error retrieving data:", error);
+    });
 
-    if (url_params.ual) {
-        console.log(v_pubs)
-    }
+  if (url_params.ual) {
+    console.log(v_pubs);
+  }
 
   res.json({
     v_pubs: v_pubs,
-    msg: ``
-  })
-})
+    msg: ``,
+  });
+});
 
-module.exports = router
+module.exports = router;
--- routes/portal/gateway.js
+++ routes/portal/gateway.js
@@ -1,135 +1,209 @@
-require('dotenv').config()
-var express = require('express')
-var router = express.Router()
-const mysql = require('mysql')
-const purl = require('url')
+require("dotenv").config();
+var express = require("express");
+var router = express.Router();
+const mysql = require("mysql");
+const purl = require("url");
 const othubdb_connection = mysql.createConnection({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
-  database: process.env.OTHUB_DB
-})
+  database: process.env.OTHUB_DB,
+});
 
-function executeOTHubQuery (query, params) {
+function executeOTHubQuery(query, params) {
   return new Promise((resolve, reject) => {
     othubdb_connection.query(query, params, (error, results) => {
       if (error) {
-        reject(error)
+        reject(error);
       } else {
-        resolve(results)
+        resolve(results);
       }
-    })
-  })
+    });
+  });
 }
 
-async function getOTHubData (query, params) {
+async function getOTHubData(query, params) {
   try {
-    const results = await executeOTHubQuery(query, params)
-    return results
+    const results = await executeOTHubQuery(query, params);
+    return results;
   } catch (error) {
-    console.error('Error executing query:', error)
-    throw error
+    console.error("Error executing query:", error);
+    throw error;
   }
 }
 
 /* GET explore page. */
-router.get('/', async function (req, res, next) {
-    try{
-            ip = req.socket.remoteAddress
+router.get("/", async function (req, res, next) {
+  try {
+    ip = req.socket.remoteAddress;
     if (process.env.SSL_KEY_PATH) {
-        ip = req.headers['x-forwarded-for']
+      ip = req.headers["x-forwarded-for"];
     }
 
-    url_params = purl.parse(req.url, true).query
+    url_params = purl.parse(req.url, true).query;
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
 
-    query = `select * from txn_header`
-    conditions = []
-    params = []
+    if (url_params.rejectTxn) {
+      query = `UPDATE txn_header set progress = 'REJECTED' where txn_id = ?`;
+      await othubdb_connection.query(
+        query,
+        [url_params.rejectTxn],
+        function (error, results, fields) {
+          if (error) throw error;
+        }
+      );
+    }
 
-    limit = 100
+    query = `select * from txn_header`;
+    conditions = [];
+    params = [];
+
+    limit = 100;
     if (url_params.limit && Number(url_params.limit)) {
-        limit = url_params.limit
+      limit = url_params.limit;
     }
 
     if (url_params.account) {
-        conditions.push(`public_address = ?`)
-        params.push(url_params.account)
+      conditions.push(`public_address = ?`);
+      params.push(url_params.account);
     }
-    
+
+    conditions.push(`network = ?`);
+    if (url_params.network == "Origintrail Parachain Testnet") {
+      params.push("otp::testnet");
+    }
+    if (url_params.network == "Origintrail Parachain Mainnet") {
+      params.push("otp::mainnet");
+    }
+
+    if (url_params.txn_id) {
+      conditions.push(`txn_id = ?`);
+      params.push(url_params.txn_id);
+    }
+
     if (url_params.app_name) {
-        conditions.push(`app_name = ?`)
-        params.push(url_params.app_name)
+      conditions.push(`app_name = ?`);
+      params.push(url_params.app_name);
     }
 
     if (url_params.ual) {
-        conditions.push(`ual = ?`)
-        params.push(url_params.ual)
+      conditions.push(`ual = ?`);
+      params.push(url_params.ual);
     }
 
-    if (url_params.order == 'minted') {
-        conditions.push(`type = ?`)
-        params.push('publish')
-
-        conditions.push(`progress = ?`)
-        params.push('COMPLETE')
+    if (url_params.progress == "COMPLETE") {
+      conditions.push(`progress = ?`);
+      params.push(url_params.progress);
     }
 
-    if (url_params.order == 'pending') {
-        conditions.push(`type = ?`)
-        params.push('publish')
-
-        conditions.push(`progress = ?`)
-        params.push('PENDING')
+    if (url_params.progress == "PENDING") {
+      conditions.push(`progress = ?`);
+      params.push(url_params.progress);
     }
 
-    conditions.push(`request in (?,?,?)`)
-    params.push('publish')
-    params.push('update')
-    params.push('transfer')
-
-    conditions.push(`network = ?`)
-    console.log(url_params.network)
-    if(url_params.network == 'Origintrail Parachain Testnet'){
-        params.push('otp::testnet')
-    }
-    if(url_params.network == 'Origintrail Parachain Mainnet'){
-        params.push('otp::mainnet')
+    if (url_params.progress == "REJECTED") {
+      conditions.push(`progress = ?`);
+      params.push(url_params.progress);
     }
 
-    order_by = 'created_at'
+    if (url_params.progress == "ALL") {
+      conditions.push(`progress in (?,?,?)`);
+      params.push("COMPLETE");
+      params.push("PENDING");
+      params.push("REJECTED");
+    }
+
+    if (
+      url_params.progress == "COMPLETE" ||
+      url_params.progress == "PENDING" ||
+      url_params.progress == "REJECTED"
+    ) {
+      conditions.push(`progress = ?`);
+      params.push(url_params.progress);
+    }
+
+    if (url_params.request == "ALL") {
+      conditions.push(`request in (?,?,?)`);
+      params.push("Mint");
+      params.push("Update");
+      params.push("Transfer");
+    }
+
+    if (
+      url_params.request == "Mint" ||
+      url_params.request == "Update" ||
+      url_params.request == "Transfer"
+    ) {
+      conditions.push(`request = ?`);
+      params.push(url_params.request);
+    }
+
+    order_by = "created_at";
     if (url_params.order_by) {
-        order_by = url_params.order_by
+      order_by = url_params.order_by;
     }
 
-    whereClause = conditions.length > 0 ? 'WHERE ' + conditions.join(' AND ') : ''
-    sqlQuery = query + ' ' + whereClause + ` order by ${order_by} desc LIMIT ${limit}`
+    whereClause =
+      conditions.length > 0 ? "WHERE " + conditions.join(" AND ") : "";
+    sqlQuery =
+      query + " " + whereClause + ` order by ${order_by} desc LIMIT ${limit}`;
 
-    txn_header = ''
-    console.log(sqlQuery)
+    console.log(sqlQuery);
     txn_header = await getOTHubData(sqlQuery, params)
-        .then(results => {
+      .then((results) => {
         //console.log('Query results:', results);
-        return results
+        return results;
         // Use the results in your variable or perform further operations
-        })
-        .catch(error => {
-        console.error('Error retrieving data:', error)
-        })
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
+
+    conditions = [];
+    params = [];
+
+    if (url_params.account) {
+      conditions.push(`public_address = ?`);
+      params.push(url_params.account);
+    }
+
+    conditions.push(`network = ?`);
+    if (url_params.network == "Origintrail Parachain Testnet") {
+      params.push("otp::testnet");
+    }
+    if (url_params.network == "Origintrail Parachain Mainnet") {
+      params.push("otp::mainnet");
+    }
+
+    whereClause =
+      conditions.length > 0 ? "WHERE " + conditions.join(" AND ") : "";
+    sqlQuery =
+      query + " " + whereClause + ` order by ${order_by} desc LIMIT ${limit}`;
+
+    raw_txn_header = await getOTHubData(sqlQuery, params)
+      .then((results) => {
+        //console.log('Query results:', results);
+        return results;
+        // Use the results in your variable or perform further operations
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
 
     res.json({
-        txn_header: txn_header,
-        msg: ``
-    })
-    }catch(e){
-        console.log(e)
-    }
-})
+      raw_txn_header: raw_txn_header,
+      txn_header: txn_header,
+      msg: ``,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-module.exports = router
+module.exports = router;
