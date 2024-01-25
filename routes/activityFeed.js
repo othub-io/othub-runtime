4b937a4c7c8cecb866839dc804599a2ffc97f065
CosmiCloud <tybranha@gmail.com>
2024-01-24 20:43:13 -0500

    try catch for actvity feed failure

--- routes/activityFeed.js
+++ routes/activityFeed.js
@@ -6,38 +6,37 @@ const queryDB = queryTypes.queryDB()
 
 /* GET explore page. */
 router.post("/", async function (req, res, next) {
-  ip = req.socket.remoteAddress;
-  if (process.env.SSL_KEY_PATH) {
-    ip = req.headers["x-forwarded-for"];
-  }
+  try{
+    timeframe = req.body.timeframe;
+    network = req.body.network;
+    blockchain = req.body.blockchain;
 
-  timeframe = req.body.timeframe;
-  network = req.body.network;
-  blockchain = req.body.blockchain;
+    if (blockchain) {
+      network = "";
+    }
 
-  if (blockchain) {
-    network = "";
-  }
+    limit = "200";
+    conditions = [];
+    params = []
 
-  limit = "200";
-  conditions = [];
-  params = []
+    query = `select signer,UAL,datetime,tokenId,transactionHash,eventName,eventValue1,chain_id from v_pubs_activity_last1min UNION ALL select tokenSymbol,UAL,datetime,tokenId,transactionHash,eventName,eventValue1,chain_id from v_nodes_activity_last1min WHERE eventName != 'StakeIncreased' order by datetime desc LIMIT ${limit}`;
 
-  query = `select signer,UAL,datetime,tokenId,transactionHash,eventName,eventValue1,chain_id from v_pubs_activity_last1min UNION ALL select tokenSymbol,UAL,datetime,tokenId,transactionHash,eventName,eventValue1,chain_id from v_nodes_activity_last1min WHERE eventName != 'StakeIncreased' order by datetime desc LIMIT ${limit}`;
+    data = await queryDB.getData(query, params, network, blockchain)
+      .then((results) => {
+        //console.log('Query results:', results);
+        return results;
+        // Use the results in your variable or perform further operations
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
 
-  data = await queryDB.getData(query, params, network, blockchain)
-    .then((results) => {
-      //console.log('Query results:', results);
-      return results;
-      // Use the results in your variable or perform further operations
-    })
-    .catch((error) => {
-      console.error("Error retrieving data:", error);
+    res.json({
+      activity_data: data,
     });
-
-  res.json({
-    activity_data: data,
-  });
+  }catch(e){
+    console.log(e)
+  }
 });
 
 module.exports = router;
