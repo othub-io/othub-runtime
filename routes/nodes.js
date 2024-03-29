c0429069cd80cce72aa975bf2417adead52cc8b8
CosmiCloud <tybranha@gmail.com>
2024-03-29 12:45:24 -0400

    prep for next release

--- routes/nodes.js
+++ routes/nodes.js
@@ -11,20 +11,38 @@ router.post("/", async function (req, res, next) {
     ip = req.headers["x-forwarded-for"];
   }
 
-  network = req.body.network;
-  blockchain = req.body.blockchain;
+  let network = req.body.network;
+  let blockchain = req.body.blockchain;
+  let node_name = req.node_name;
 
-  if(blockchain){
-    network = ""
+  if (blockchain) {
+    network = "";
   }
 
-  order_by = "nodeStake"
-  if(req.body.order_by){
+  order_by = "nodeStake";
+  if (req.body.order_by) {
     order_by = req.body.order_by;
   }
 
-  query = `select * from v_nodes where nodeStake >= 50000 AND nodeId != '0' order by ${order_by} asc`;
+  let query = `select * from v_nodes`;
+  let conditions = [];
   let params = [];
+
+  conditions.push(`nodeStake >= ?`);
+  params.push(50000);
+
+  conditions.push(`nodeId != ?`);
+  params.push(0);
+
+  if (node_name) {
+    conditions.push(`tokenName != ?`);
+    params.push(0);
+  }
+
+  whereClause =
+    conditions.length > 0 ? "WHERE " + conditions.join(" AND ") : "";
+  query = query + " " + whereClause + ` order by ${order_by} asc`;
+
   nodes = await queryDB
     .getData(query, params, network, blockchain)
     .then((results) => {
@@ -36,41 +54,38 @@ router.post("/", async function (req, res, next) {
       console.error("Error retrieving data:", error);
     });
 
-    network = ""
-    let node_list = []
-    for(const node of nodes){
-      blockchain = node.chainName
-      query = `select estimatedEarnings,cumulativePayouts from v_nodes_stats_latest where nodeId = ?`;
-      params = [node.nodeId];
-      dkg_node = await queryDB
-        .getData(query, params, network, blockchain)
-        .then((results) => {
-          //console.log('Query results:', results);
-          return results;
-          // Use the results in your variable or perform further operations
-        })
-        .catch((error) => {
-          console.error("Error retrieving data:", error);
-        });
+  network = "";
+  let node_list = [];
+  for (const node of nodes) {
+    blockchain = node.chainName;
+    query = `select shareValueCurrent from v_nodes_stats_latest where nodeId = ?`;
+    params = [node.nodeId];
+    dkg_node = await queryDB
+      .getData(query, params, network, blockchain)
+      .then((results) => {
+        //console.log('Query results:', results);
+        return results;
+        // Use the results in your variable or perform further operations
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
 
-      let after_fee_earnings = dkg_node[0].estimatedEarnings - (dkg_node[0].estimatedEarnings * (node.nodeOperatorFee / 100))
-      let shareValue = (node.nodeStake + after_fee_earnings - dkg_node[0].cumulativePayouts) / node.nodeStake
-      
-      node_obj = {
-        chainId: node.chainId,
-        chainName: node.chainName,
-        nodeId: node.nodeId,
-        tokenName: node.tokenName,
-        tokenSymbol: node.tokenSymbol,
-        nodeStake: node.nodeStake,
-        nodeOperatorFee: node.nodeOperatorFee,
-        nodeAsk: node.nodeAsk,
-        nodeAgeDays:node.nodeAgeDays,
-        shareValue: shareValue
-      }
+    node_obj = {
+      chainId: node.chainId,
+      chainName: node.chainName,
+      nodeId: node.nodeId,
+      tokenName: node.tokenName,
+      tokenSymbol: node.tokenSymbol,
+      nodeStake: node.nodeStake,
+      nodeOperatorFee: node.nodeOperatorFee,
+      nodeAsk: node.nodeAsk,
+      nodeAgeDays: node.nodeAgeDays,
+      shareValueCurrent: dkg_node[0].shareValueCurrent,
+    };
 
-      node_list.push(node_obj)
-    }
+    node_list.push(node_obj);
+  }
 
   res.json({
     nodes: node_list,
